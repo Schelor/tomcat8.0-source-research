33181d0662434e394a9ceac39fe6254fcd5f76d3
fdx321 <fdx321@126.com>
2017-07-17 13:12:08 +0800

    modify demo

--- springmvc-demo/src/main/java/com/fdx/HelloWorld.java
+++ springmvc-demo/src/main/java/com/fdx/HelloWorld.java
@@ -5,12 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HelloWorld{
 
     @ResponseBody
     @RequestMapping(value="/hello", method = RequestMethod.GET)
-    public String hello() {
+    public String hello(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        session.getAttribute("userId");
         return "{\"key\":\"value\"}";
     }
 }
